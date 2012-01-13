ec24f00fc5a41323236225408f5f94261f517863
Steven Lotito <slotito@zappos.com>
2012-01-13 11:11:32 -0800

    Working on rendering

--- amazed/draw2D.m
+++ amazed/draw2D.m
@@ -31,37 +31,83 @@ void initializeMaze(int size, int maze[size][size])
 }
 
 void drawMaze(CGContextRef context, int x_offset, int y_offset, int cell_size_px, int size, int maze[size][size]){
-    // N, S, E, W = 1, 2, 4, 8
+    // N=1
+    // S=2
+    // E=4
+    // W=8
     for(int y = 0; y < size; y ++){
         for(int x = 0; x < size; x++){
+            // draw complete grid
+            CGRect rectangle = CGRectMake(x_offset + (x*cell_size_px),
+                                          y_offset + (y*cell_size_px),
+                                          cell_size_px,
+                                          cell_size_px);
+            CGContextAddRect(context, rectangle);
+        }
+    }
+    
+    CGContextStrokePath(context);
+    
+    for(int y = 0; y < size; y ++){
+        
+        for(int x = 0; x < size; x++){
+            
+            CGContextSetStrokeColorWithColor(context, 
+                                             [UIColor whiteColor].CGColor);
+            
             int drawingValue = maze[y][x];
+            
             NSLog(@"[%d][%d] = %d", x, y, drawingValue);
-            if( drawingValue == 0 ){
-                // do nothing
-            } else if( drawingValue == 1){
-                CGRect rectangle = CGRectMake(x_offset + (x*cell_size_px),
-                                              y_offset + (y*cell_size_px),
-                                              cell_size_px,
-                                              cell_size_px);
-                CGContextAddRect(context, rectangle);
+            
+            int thisX = x_offset + x*cell_size_px;
+            int thisY = y_offset + y*cell_size_px;
+            
+            CGContextMoveToPoint(context, thisX, thisY);
+            
+            int top = thisY;
+            int bot = thisY + cell_size_px;
+            
+            int lhs = thisX;
+            int rhs = thisX + cell_size_px;
+            
+//            NSLog(@"cell is [%d,%d],[%d,%d]  [%d,%d],[%d,%d]", lhs, top, lhs, bot, rhs, top, rhs, bot);
+            
+            if( drawingValue == 1){
+                // draw the opening at top
+
+                CGContextAddLineToPoint( context, rhs, top);
+                CGContextStrokePath(context);
+//                
+//                CGContextAddLineToPoint( context, rhs, bot);
+//                CGContextStrokePath(context);
+//                
+//                CGContextAddLineToPoint( context, rhs, top);
+//                CGContextStrokePath(context);
             } else if( maze[y][x] == 2 ){
-                CGRect rectangle = CGRectMake(x_offset + (x*cell_size_px),
-                                              y_offset + (y*cell_size_px),
-                                              cell_size_px,
-                                              cell_size_px);
-                CGContextAddRect(context, rectangle);
+                // draw the opening at bottom
+
             } else if( drawingValue == 4){
-                CGRect rectangle = CGRectMake(x_offset + (x*cell_size_px),
-                                              y_offset + (y*cell_size_px),
-                                              cell_size_px,
-                                              cell_size_px);
-                CGContextAddRect(context, rectangle);
+                //draw opening at right
+
+                // switch to white and 'erase' one wall
+
+
+                CGContextMoveToPoint(context, rhs, top);
+                
+                CGContextAddLineToPoint( context, rhs, bot);
+                
+                CGContextStrokePath(context);
+
+                
             } else if( maze[y][x] == 8 ){
-                CGRect rectangle = CGRectMake(x_offset + (x*cell_size_px),
-                                              y_offset + (y*cell_size_px),
-                                              cell_size_px,
-                                              cell_size_px);
-                CGContextAddRect(context, rectangle);
+                // draw opening at left
+
+                CGContextMoveToPoint(context, lhs, top);
+                
+                CGContextAddLineToPoint( context, lhs, bot);
+                
+                CGContextStrokePath(context);
+
             } else {
                 // oops
             }
@@ -72,8 +118,9 @@ void drawMaze(CGContextRef context, int x_offset, int y_offset, int cell_size_px
 void carvePassages(int currentX, int currentY, int size, int maze[size][size])
 {
     int exp = arc4random_uniform(3);
-    int value = pow(2,exp);
+    int value = pow(2,exp); // 1,2,4,8
     maze[currentX][currentY] = value;
+
     if( currentX + 1 < size){
         carvePassages(currentX + 1, currentY, size, maze);
     }
@@ -98,7 +145,9 @@ void carvePassages(int currentX, int currentY, int size, int maze[size][size])
 
     initializeMaze(maze_size, maze);
     
-    carvePassages(0, 0, maze_size, maze);
+    for(int y = 0; y < maze_size; y++){
+        carvePassages(0, y, maze_size, maze);
+    }
     
     drawMaze(context, x_offset, y_offset, cell_size_px, maze_size, maze);
 
